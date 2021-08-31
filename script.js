9f3d4211417da4b069614debeefeb4a4aa755828
Ilie531 <trifanilie2002@gmail.com>
2021-08-31 10:57:08 +0200

    Exercise in script

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ document.write //don't usually use this. It removes everything written down in H
 
 
 */
-let a,b,c ;
+/*let a,b,c ;
 a=5;
 b=1;
 c= document.getElementById();//when you want to get id and put it in window.alert
 
 var a ;//can make empty variable with this
 var a= "something";
+*/
 
 //Data types
 /*
@@ -56,4 +57,27 @@ Object for now won't use objects or data types
 var car {mark=...}
 
 
+
+//loop for in (will return the position in the array)
+for (x in list) { window.alert(x);};
+sugar, spice, pretty things - with y
+ 0       1           2 - with x
+for (y of list)
+
+while (z<1o) { window.alert(z); z++};
+
+
+//function syntax
+funtion FunctionTest (x,y) {return x+y;};
+
+give all par numbers between 2 numbers in 2 variables
+% of all numbers between x & y with window.alert
 */
+
+var x=10;
+var y=28;
+
+for (let i=x; i<=y; i++) 
+{
+    if(i%2==0) window.alert(i);
+};
