8aad57feff1c09a789728ee867ea953d827c6bd5
Ilie531 <trifanilie2002@gmail.com>
2021-08-31 10:30:56 +0200

    Added some java code

---
+++ script.js
@@ -0,0 +1,59 @@
+/*
+document.getElementById. //take the id element in the HTML
+.innerHTML//change the content of text in the id
+
+window.alert(not working)
+console.log
+document.write //don't usually use this. It removes everything written down in HTML or CSS
+//This is a comment
+
+
+*/
+let a,b,c ;
+a=5;
+b=1;
+c= document.getElementById();//when you want to get id and put it in window.alert
+
+var a ;//can make empty variable with this
+var a= "something";
+
+//Data types
+/*
+string
+numbers
+boolean - true or false
+for string
+var a="  " or '  '
+
+
+Operators
++
+-
+*
+** exponentiale
+/
+%
+++ increment
+-- decrement
+
+Comparator
+== 2 things are equal
+=== 2 things are equal and the same type
+!= not equal
+!== not equal or not same type
+>
+>=
+<
+<=
+
+
+logical operators
+and - &&
+|| - or
+! - not
+
+Object for now won't use objects or data types
+var car {mark=...}
+
+
+*/
